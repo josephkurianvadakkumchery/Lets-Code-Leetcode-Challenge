acf02cc0df3377bbf637d3a2d2d34ab4cbaba9b0
josephkurianvadakkumchery <josephvadakkumchery2007@gmail.com>
2026-02-25 19:21:20 +0530

    Delete Day3 directory

--- Day3/TempConvert.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        if(0<=celsius and celsius<=1000):
-            kel=celsius+273.15
-            far=celsius*1.80+32.00
-            tem=[kel,far]
-            return tem
