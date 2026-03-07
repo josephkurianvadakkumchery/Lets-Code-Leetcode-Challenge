bf990ebbc26017da390d98edce7664726e81a457
josephkurianvadakkumchery <josephvadakkumchery2007@gmail.com>
2026-03-08 00:09:20 +0530

    Create sumevencount.py

---
+++ Day13/sumevencount.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+        for i in range(1, num + 1):
+            digit_sum = sum(int(d) for d in str(i))
+            if digit_sum % 2 == 0:
+                count += 1
+        return count
