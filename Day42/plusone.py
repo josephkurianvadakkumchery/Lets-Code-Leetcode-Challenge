1c06cd83e3ecf8126a6c383e28ea21c37826d3e7
josephkurianvadakkumchery <josephvadakkumchery2007@gmail.com>
2026-04-05 18:12:53 +0530

    Create plusone.py

---
+++ Day42/plusone.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = int("".join(map(str,digits))) + 1
+        result = [int(digit) for digit in str(num)]
+        return result
