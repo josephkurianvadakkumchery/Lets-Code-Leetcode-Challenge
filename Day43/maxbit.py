3cd964fd2ff548b80b4f28519705416c3c1a7f92
josephkurianvadakkumchery <josephvadakkumchery2007@gmail.com>
2026-04-08 10:14:35 +0530

    Create maxbit.py

---
+++ Day43/maxbit.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones_count = s.count('1')
+        zeros_count = s.count('0')
+        return '1' * (ones_count - 1) + '0' * zeros_count + '1'
